4126269693c3ab018a7dcbeb411f502b1f96d9a1
kumar <krupavenkatkumarbommana42566@gmail.com>
2026-07-13 14:25:52 +0530

    Updated respones

--- src/Utils/Responeses.go
+++ src/Utils/Responeses.go
@@ -2,15 +2,15 @@ package utils
 
 import "github.com/gofiber/fiber/v3"
 
-func Error(c fiber.Ctx, Status int,message string) error{
-	return c.Status(Status).JSON(fiber.Map{
+func Error(c fiber.Ctx, StatusCode int,message string) error{
+	return c.Status(StatusCode).JSON(fiber.Map{
 			"success": false,
 			"message":message,
 			})
 }
 
-func Success (c fiber.Ctx,Status int,message string,data interface{}) error{
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+func Success (c fiber.Ctx,StatusCode int,message string,data interface{}) error{
+	return c.Status(StatusCode).JSON(fiber.Map{
 		"success": true,
 		"message":message,
 		"User":data,
